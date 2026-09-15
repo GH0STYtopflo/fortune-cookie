2c6a2ade60caecd462f39cff75471febe92ef00c
Sepehr <soapss85@proton.me>
2026-09-15 17:31:48 +0330

    fix: Add some routes to the auth middleware

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,18 @@ Route::get('/', function () {
 Route::view('/signup', 'signup', ['title' => 'Signup']);
 Route::post('/signup', [UserController::class, 'store']);
 
-Route::view('/login', 'login', ['title' => 'Login']);
+Route::view('/login', 'login', ['title' => 'Login'])->name('login');
 Route::post('/login', [UserController::class, 'login']);
 
-Route::post('/logout', [UserController::class, 'logout']);
+Route::group(['middleware' => 'auth'], function () {
+    Route::post('/logout', [UserController::class, 'logout']);
 
-Route::get('/get_cookie', [UserFortuneController::class, 'addFortune']);
-Route::get('/my_cookies', [UserFortuneController::class, 'index']);
+    Route::get('/get_cookie', [UserFortuneController::class, 'addFortune']);
+    Route::get('/my_cookies', [UserFortuneController::class, 'index']);
 
-Route::delete('/my_cookies/{fortune}', [UserFortuneController::class, 'destroy']);
+    Route::delete('/my_cookies/{fortune}', [UserFortuneController::class, 'destroy']);
+});
+
+Route::get('test/{param}', function ($param) {
+   return $param;
+})->whereNumber('param');
