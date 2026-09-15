25c1edc3190d282a81bd3f70507e31c60513f1a6
Sepehr <soapss85@proton.me>
2026-09-15 11:11:52 +0330

    feat: Add feature to query fortunes

--- app/Http/Controllers/UserFortuneController.php
+++ app/Http/Controllers/UserFortuneController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Fortune;
 use App\Models\User;
 use Auth;
+use Illuminate\Http\Request;
 
 class UserFortuneController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        if (!is_null($request->query('body'))) {
+            $fortunes = Auth::user()->fortunes()->where('body', 'LIKE', '%' . $request->query('body') . '%')->get();
+
+            return view('cookies', ['cookies' => $fortunes]);
+        }
+
         return view('cookies', ['cookies' => Auth::user()->fortunes]);
     }
 
--- resources/views/cookies.blade.php
+++ resources/views/cookies.blade.php
@@ -1,5 +1,11 @@
 <x-layout title="Your cookies 🍪">
-    <div class="w-full items-center flex flex-col mt-25 mb-50">
+    <div class="w-full items-center flex flex-col mt-20 mb-50">
+        <form action="/my_cookies" method="GET" style="background-color: #1B192A" class="p-10 rounded-2xl w-1/3 flex flex-col items-center mb-5">
+            <label class="font-bold text-2xl" style="color: white">Search Your Cookies</label>
+            <br>
+            <input name="body" type="text" class="bg-white w-100 h-10 p-2 rounded" placeholder="Cookie Text">
+        </form>
+
         @foreach($cookies as $cookie)
             <x-cookie_card :date="$cookie->created_at" :content="$cookie->body" :id="$cookie->id"/>
         @endforeach
